0bbbc691ceeae900af660ea273de67d370e54936
Francis Boateng <francisboateng5202@gmail.com>
2026-06-17 19:16:43 -0700

    connected database to TiDB severless and cors resolution

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors( {
+origin: 'https://relayai-usaii.vercel.app', // Adjust this if your frontend runs on a different origin
+methods: ['GET', 'POST', 'PUT', 'DELETE'],
+allowedHeaders: ['Content-Type', 'Authorization']
+
+}));
+
+
 app.use(express.json());
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
